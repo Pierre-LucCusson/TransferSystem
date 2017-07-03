b1fefe5cf54c9a51bd4110b78dd72c418096454b
Pierre-Luc Cusson <ak37560@ens.etsmtl.ca>
2017-07-03 18:26:17 -0400

    add qr scan and contact list

---
+++ app/src/main/java/ets/transfersystem/Contacts.java
@@ -0,0 +1,55 @@
+package ets.transfersystem;
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Pierre-Luc on 2017-06-29.
+ */
+
+public class Contacts {
+
+    private SharedPreferences settings;
+
+    public Contacts(SharedPreferences settings) {
+        this.settings = settings;
+//        settings = getSharedPreferences("Contacts", 0);
+    }
+
+    public void saveContact(String information) {
+        String deviceID = information + " oneDeviceID";
+        String ipAddresse = information + " oneipAddresse";
+        List<String> ets = Arrays.asList(deviceID, ipAddresse);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(deviceID, information);
+        editor.commit();
+    }
+
+    public void saveContacts(String deviceID, String ipAddresse) {
+        String information = deviceID + ":" + ipAddresse;
+        List<String> ets = Arrays.asList(deviceID, ipAddresse);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(deviceID, information);
+        editor.commit();
+    }
+
+    public String getContact(String deviceID) {
+        return settings.getString(deviceID, "");
+    }
+
+    public void printAll() {
+        Map<String, ?> x = settings.getAll();
+        Log.d("Contacts", String.format(x.toString()));
+    }
+
+    public String[] getAllContacts() {
+        Map<String, ?> mapsContacts = settings.getAll();
+        return mapsContacts.values().toArray(new String[0]);
+    }
+
+}
--- app/src/main/java/ets/transfersystem/ContactsActivity.java
+++ app/src/main/java/ets/transfersystem/ContactsActivity.java
@@ -1,11 +1,9 @@
 package ets.transfersystem;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Pierre-Luc on 2017-06-26.
@@ -18,22 +16,13 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        //Back button
-        final Button backButton = (Button) findViewById(R.id.backButton);
-        backButton.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-                back(view);
-            }
-        } );
+        Contacts contacts = new Contacts(getSharedPreferences("ContactsTest", 0));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_list, contacts.getAllContacts());
+
+        ListView list = (ListView) findViewById(R.id.contacts_list);
+        list.setAdapter(adapter);
+
 
     }
-
-    public void back(View view) {
-        Button button = (Button) view;
-        Log.d("ButtonClick", String.format("Back button from contacts was click"));
-        startActivity(new Intent(ContactsActivity.this, MainActivity.class));
-    }
-
 }
