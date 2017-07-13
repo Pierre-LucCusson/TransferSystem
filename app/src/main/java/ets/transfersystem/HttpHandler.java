04a4dbec5bb4064da54e3848e3d447b4a4feff69
Gabriel Trahan <godmaster017@gmail.com>
2017-07-13 14:32:42 -0400

    server and client

---
+++ app/src/main/java/ets/transfersystem/HttpHandler.java
@@ -0,0 +1,40 @@
+package ets.transfersystem;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+
+/**
+ * Created by gab on 13/07/17.
+ */
+
+public class HttpHandler extends Service {
+
+    IBinder binder;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public String sendLongPolling()
+    {
+        ClientLP client = new ClientLP();
+        try {
+            return client.sendLongPolling();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
